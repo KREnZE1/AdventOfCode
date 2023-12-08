f48cca59afdc85835b35b2d12dab00e7d079f1c4
KREnZE <r.henze06@web.de>
2023-12-08 13:43:27 +0100

    Added correct parsing for 2023/03

--- Years/Y2023/Day_03/Main.java
+++ Years/Y2023/Day_03/Main.java
@@ -3,12 +3,11 @@ package Years.Y2023.Day_03;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
 
     static final String INPUTLOC = "Years"+java.io.File.separator+"Y2023"+java.io.File.separator+"Day_03"+java.io.File.separator+"input.txt";
-    static ArrayList<String> input = new ArrayList<>();
+    static char[][] input = new char[140][];
 
     public static void main(String[] args) {
         readInput();
@@ -19,16 +18,18 @@ public class Main {
     public static void readInput() {
         try (BufferedReader br = new BufferedReader(new FileReader(INPUTLOC))) {
             String temp;
-            while ((temp = br.readLine()) != null) input.add(parse(temp));
+            int i=0;
+            while ((temp = br.readLine()) != null) {
+                input[i++] = parse(temp);
+            }
         } catch(IOException ioe) {
             System.err.println(ioe.getLocalizedMessage());
             System.err.println(ioe.getStackTrace());
         }
     }
 
-    private static String parse(String in) {
-        //TODO: Implement
-        return in;
+    private static char[] parse(String in) {
+        return in.toCharArray();
     }
 
     public static void solvePartOne() {
