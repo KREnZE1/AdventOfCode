e58b08e797f5ce1feadb100d53d11e8b8fdd03d9
KREnZE <r.henze06@web.de>
2023-12-08 11:50:08 +0100

    Untested, but structure creation logic could work already

--- Utilities/CreateStructure.java
+++ Utilities/CreateStructure.java
@@ -1,12 +1,17 @@
 package Utilities;
 
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CreateStructure {
 
-
     public static void main(String[] args) {
-        if (args.length<2) printHelpAndExit();
+        if (args.length < 2)
+            printHelpAndExit();
         switch (args[0]) {
             case "Year":
                 createYearStructure(args[1]);
@@ -22,9 +27,9 @@ public class CreateStructure {
     }
 
     public static void createYearStructure(String yearNum) {
-        final String basePath = "Years"+File.separator+"Y"+yearNum+File.separator;
+        final String basePath = "Years" + File.separator + "Y" + yearNum + File.separator;
 
-        for (int i=1; i<26; i++) {
+        for (int i = 1; i < 26; i++) {
             String childName = convertToDayName(i);
             File newDir = new File(basePath, childName);
             if (newDir.exists()) {
@@ -40,20 +45,65 @@ public class CreateStructure {
 
     private static String convertToDayName(int daynum) {
         String output = "Day_";
-        if (daynum<10) output += "0"+Integer.toString(daynum);
-        else output += Integer.toString(daynum);
+        if (daynum < 10)
+            output += "0" + Integer.toString(daynum);
+        else
+            output += Integer.toString(daynum);
         return output;
     }
 
-    private static void createInput(String parentPath) {
+    private static File createFile(String parentPath, String childName) {
+        File file = new File(parentPath, childName);
+        try {
+            file.createNewFile();
+        } catch (IOException ioe) {
+            System.err.println(ioe.getLocalizedMessage());
+            System.err.println(ioe.getStackTrace());
+            System.exit(-1);
+        }
+        return file;
+    }
 
+    private static void createInput(String parentPath) {
+        createFile(parentPath, "input.txt");
+        //TODO: Automatically fetch input content from the website?
+        //Would require logging in, but could then basically save the newly opened website as a text file, because the entire content is relevant
     }
 
     private static void createTestInput(String parentPath) {
-
+        createFile(parentPath, "testInput.txt");
+        //TODO: Automatically fetch testInput content from the website?
+        //Would not require logging in because the test cases are the same for every participant, but would require a lot of parsing because it is not uniformly positioned
     }
 
     private static void createJavaClass(String parentPath) {
-        
+        File curr = createFile(parentPath, "Main.java");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(curr));
+                BufferedReader br = new BufferedReader(new FileReader("Utilities" + File.separator + "Example.txt"))) {
+
+            String temp;
+            for (int i=0; i<42; i++) {
+                temp = br.readLine();
+                if (i == 0) {
+                    String begin = temp.substring(0, 15); //Should contain everything up to the Y in the year num
+                    String year = curr.getPath().substring(7, 11); //Should contain the year number
+                    String mid = temp.substring(19, 24); //Should contain .Day_
+                    String day = curr.getPath().substring(16, 18); //Should contain the day number
+                    bw.write(begin+year+mid+day+";\n");
+                } else if (i == 9) {
+                    String begin = temp.substring(0, 68);
+                    String year = curr.getPath().substring(7, 11); //Should contain the year number
+                    String mid = temp.substring(72, 102);
+                    String day = curr.getPath().substring(16, 18); //Should contain the day number
+                    String end = temp.substring(104);
+                    bw.write(begin+year+mid+day+end+"\n");
+                } else bw.write(temp+"\n");
+            }
+
+        } catch (IOException ioe) {
+            System.err.println(ioe.getLocalizedMessage());
+            System.err.println(ioe.getStackTrace());
+            System.exit(-1);
+        }
     }
 }
